5173c23607a5d3b7a056036f52aa527703e64b5b
UB3R2007 <umairkazi2007@gmail.com>
2024-07-08 23:24:00 -0400

    add app functionality, control angles

--- KeyboardController.py
+++ KeyboardController.py
@@ -2,15 +2,59 @@ import keyboard
 import time
 import serial
 import pygame
+import sys
 
 pygame.init()
-pygame.display.set_mode((400, 800))
-running = True
+screen = pygame.display.set_mode((300, 250))
+font = pygame.font.Font(None, 40)
 
-while running:
+class RoboticArm:
+    def __init__(self):
+        self.angles = [0, 0, 0, 0, 0, 0]
+        self.motor = 0
+        self.last_update_time = pygame.time.get_ticks()
+
+    def draw(self, screen):
+        # if I want to draw arm on the screen
+        pass
+
+    def handle_keypress(self, keys):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_update_time > 10:
+            if keys[pygame.K_RIGHT] and self.angles[self.motor] < 179: self.angles[self.motor] += 1
+            elif keys[pygame.K_LEFT] and self.angles[self.motor] > 1: self.angles[self.motor] -= 1
+            elif keys[pygame.K_1]: self.motor = 0
+            elif keys[pygame.K_2]: self.motor = 1
+            elif keys[pygame.K_3]: self.motor = 2
+            elif keys[pygame.K_4]: self.motor = 3
+            elif keys[pygame.K_5]: self.motor = 4
+            elif keys[pygame.K_6]: self.motor = 5
+
+            self.last_update_time = current_time
+
+    def display_angles(self, screen):
+        for i, angle in enumerate(self.angles):
+            text = font.render(f"Motor {i + 1}: {angle} degrees", True, (0, 0, 0))
+            screen.blit(text, (10, i * 40))
+
+arm = RoboticArm()
+clock = pygame.time.Clock()
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
+    
+    keys = pygame.key.get_pressed()
+    arm.handle_keypress(keys)
+
+    screen.fill((255, 255, 255))
+    # arm.draw(screen)
+    arm.display_angles(screen)
+    pygame.display.flip()
+
+    clock.tick(60)
 
 
 # # Initialize your serial connection here
